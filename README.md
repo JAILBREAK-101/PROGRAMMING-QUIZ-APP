13271f36622e813f416474c2d9822ea96d0b2cc2
Oluwagbemiga Awosope <100247352+JAILBREAK-101@users.noreply.github.com>
2023-02-22 13:13:01 +0100

    Added files for Web Application

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# QUIZ-PROJECT-APP
-This is a Quiz App, that lets you answer multiple question, and shows your score after. It is a Programming quiz app, concentrated on Programming alone (enjoy). 
+# FEATURES TO ADD
+
+- More Programming Languages and programming questions for each languages added.
+- Backend to make user login as an admin to use the app
+
+- Correction (Dialog) for each question missed to enable students to be able to understand what they did wrong, and also try not to make the same mistake. -- Get explaination for question from We
+
+- More features that gives user control over the app, like : Suggestions for questions to be added section. Adding past questions, and turning it into a school app, as well as programming.
+
+## THIS IS A QUIZ APP MADE WITH JAVASCRIPT, THE MARKUP LANGUAGE (HTML), AND CSS
+
+## THE INSTRUCTIONS OF THE QUIZ GAME
---
+++ js/java.js
@@ -0,0 +1,331 @@
+// FOR JAVA
+let questions_and_options2 = [
+    {
+    question : "What is a correct syntax to output 'Hello World'in Java?",
+    answers: [
+            {
+                text : 'console.log("Hello World")', 
+                correct : false
+            },
+            {
+                text : 'System.out.println("Hello World");', 
+                correct : true
+            },
+            {
+                text : 'Console.WriteLine("Hello World");', 
+                correct : false
+            },
+            {
+                text : 'cout << "Hello World"; <<', 
+                correct : false
+            }
+        ]
+    },
+    {
+        question : `How do you insert COMMENTS in Java code?`,
+        answers : [
+            {
+                text : '#A comment',
+                correct: false
+            },
+            {
+                text : '//A comment',
+                correct : true
+            },
+            {
+                text : '/* A comment',
+                correct : false
+            },
+            {
+                text : '<!-- A comment -->',
+                correct : false
+            }
+        ]
+    },
+    {
+        question : `How do you create a variable with the numeric value 15?`,
+        answers : [
+            {
+                text : 'x = 15',
+                correct: false
+            },
+            {
+                text : 'float x = 15',
+                correct : false
+            },
+            {
+                text : 'int x = 15',
+                correct : true
+            },
+            {
+                text : 'num x = 15',
+                correct : false
+            }
+        ]
+    },
+    {
+        question : `Which method can be used to find the length of a string?`,
+        answers : [
+            {
+                text : 'len()',
+                correct: false
+            },
+            {
+                text : 'length()',
+                correct : true
+            },
+            {
+                text : 'getSize()',
+                correct : false
+            },
+            {
+                text : 'getLength()',
+                correct : false
+            }
+        ]
+    },
+    {
+        question : `The value of a string variable can be surrounded by single quotes.`,
+        answers : [
+            {
+                text : 'false',
+                correct: true
+            },
+            {
+                text : 'maybe',
+                correct : false
+            },
+            {
+                text : "I don't know",
+                correct : false
+            },
+            {
+                text : 'true',
+                correct : false
+            }
+        ]
+    },
+    {
+        question : `Which method can be used to return a string in upper case letters?`,
+        answers : [
+            {
+                text : 'touppercase{};',
+                correct: false
+            },
+            {
+                text : 'change_to_upper();',
+                correct : false
+            },
+            {
+                text : 'toUpperCase()',
+                correct : true
+            },
+            {
+                text : 'upper()',
+                correct : false
+            }
+        ]
+    },
+    {
+        question : `Which operator can be used to compare two values?`,
+        answers : [
+            {
+                text : '><',
+                correct: false
+            },
+            {
+                text : '>=',
+                correct : false
+            },
+            {
+                text : '==',
+                correct : true
+            },
+            {
+                text : '===',
+                correct : false
+            }
+        ]
+    },
+    {
+        question : `How do you insert COMMENTS in Java code?`,
+        answers : [
+            {
+                text : '#A comment',
+                correct: false
+            },
+            {
+                text : '//A comment',
+                correct : true
+            },
+            {
+                text : '/* A comment',
+                correct : false
+            },
+            {
+                text : '<!-- A comment -->',
+                correct : false
+            }
+        ]
+    },
+    {
+        question : `How do you declare an array in Java, `,
+        answers : [
+            {
+                text : `int {10} my_array [
+                    2,4,5,6,7
+                ]`,
+                correct: false
+            },
+            {
+                text : `int [10] my_array {
+                    2,4,5,6,7
+                }`,
+                correct : true
+            },
+            {
+                text : `int my_array create_array(){ 
+                    2,4,5,6,7
+                }`,
+                correct : false
+            },
+            {
+                text : `let my_array = [2,4,5,6,7]`,
+                correct : false
+            }
+        ]
+    },
+    {
+        question : `Array indexes start with:`,
+        answers : [
+            {
+                text : '1',
+                correct: false
+            },
+            {
+                text : 'start',
+                correct : false
+            },
+            {
+                text : 'begin',
+                correct : false
+            },
+            {
+                text : '0',
+                correct : true
+            }
+        ]
+    },
+    {
+        question : `Which keyword is used to create a class in Java?`,
+        answers : [
+            {
+                text : 'className',
+                correct: false
+            },
+            {
+                text : 'class',
+                correct : true
+            },
+            {
+                text : 'class()',
+                correct : false
+            },
+            {
+                text : 'MyClass',
+                correct : false
+            }
+        ]
+    },
+    {
+        question : `What is the correct way to create an object called myObj of MyClass?`,
+        answers : [
+            {
+                text: `class MyClass = new myObj()`,
+                correct : true
+            },
+            {
+                text: `class myObj = new MyClass()`,
+                correct : false
+            },
+            {
+                text: `new MyClass = myObj()`,
+                correct : false
+            },
+            {
+                text: `new myObj = MyClass()`,
+                correct : false
+            }
+        ]   
+    },
+    {
+        question : `In Java, it is possible to inherit attributes and methods from one class to another.`,
+        answers : [
+            {
+                text: `Not everytime`,
+                correct : false
+            },
+            {
+                text: `Sometimes it is, sometimes it is not`,
+                correct : false
+            },
+            {
+                text: `Yes it is possible`,
+                correct : true
+            },
+            {
+                text: `No, it is not possible`,
+                correct : false
+            }
+        ]   
+    },
+    {
+        question : `Which method can be used to find the highest value of x and y?`,
+        answers : [
+            {
+                text: `Math.largest(x,y)`,
+                correct : false
+            },
+            {
+                text: `Math.maxNum(x,y)`,
+                correct : false
+            },
+            {
+                text: `Math.maximum(x,y)`,
+                correct : false
+            },
+            {
+                text: `Math.max(x,y)`,
+                correct : true
+            }
+        ]   
+    },
+    {
+        question : `How do you start writing an if statement in Java?`,
+        answers : [
+            {
+                text: `if (x > y) {
+                    System.out.println(x + "is greater than y")
+                }`,
+                correct : true
+            },
+            {
+                text: `if x > y then: 
+                    System.out.println(x + "is greater than y")`,
+                correct : false
+            },
+            {
+                text: `if x > y: {
+                    System.out.println(x + "is greater than y")
+                }`,
+                correct : false
+            },
+            {
+                text: `if (x > y):
+                    System.out.println(x + "is greater than y")
+                `,
+                correct : false
+            }
+        ]   
+    },
+]
